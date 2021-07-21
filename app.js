a37b4871dbce363445892211857ac295cc9ee42b
t0kar <81991067+t0kar@users.noreply.github.com>
2021-07-21 12:26:00 +0200

    New functions added
    Functions for loader and error message added. Minor fixes.
    
    In progress:
    Show message in DOM if error code is 403. Only in console for now.

--- app.js
+++ app.js
@@ -2,24 +2,39 @@ const searchForm = document.querySelector("[data-search-form]");
 const searchInput = searchForm.querySelector("[name=search]");
 
 function createElement(userName, userAvatar, userPage) {
-    const resultElement = document.createElement("div"); 2
-    resultElement.innerHTML = `<img src="${userAvatar}" height = "20px" width = "20px"><a href="${userPage}">${userName}<\a>`;
+    const resultElement = document.createElement("div");
+    resultElement.innerHTML = `<img src="${userAvatar}" height="20px" width="20px"><a href="${userPage}">${userName}<\a>`;
     console.log(resultElement);
     return resultElement;
-}
+};
 
 function showLoader() {
     const loader = document.querySelector(".lds-facebook");
     loader.style.display = "inline-block";
-}
+};
 
 function noLoader() {
     const loader = document.querySelector(".lds-facebook");
     loader.style.display = "none";
-}
+};
+
+function showError(code) {
+    const error = document.querySelector("[data-result-error");
+    if (code === 403) {
+        error.innerText = "To many requests! Try again later.";
+    } else {
+        error.innerText = "Fetch error! Check console for more info.";
+    }
+};
+
+function noError() {
+    const error = document.querySelector("[data-result-error");
+    error.innerText = "";
+};
 
 searchForm.addEventListener("keyup", (event) => {
     event.preventDefault();
+    const searchListElement = document.querySelector("[data-result-list]");
 
     if (searchInput.value.length > 0) {
 
@@ -29,29 +44,34 @@ searchForm.addEventListener("keyup", (event) => {
         fetch(endpoint)
             .then((response) => {
                 showLoader();
-                return response.json()
+                noError();
+                if (response.status === 200) {
+                    console.log("Ok!");
+                    return response.json();
+                } else if (response.status === 403) {
+                    throw console.error("Too many requests! Try again later.");;
+                };
             })
             .then((data) => {
-                console.log(data.items);
-
                 noLoader();
-
-                searchListElement = document.querySelector("[data-result-list]");
+                noError();
                 searchListElement.innerHTML = "";
-
-                console.log(data.items.length);
 
                 for (let i = 0; i < data.items.length; i++) {
                     const userName = data.items[i].login;
                     const userAvatar = data.items[i].avatar_url;
                     const userPage = data.items[i].url;
                     searchListElement.appendChild(createElement(userName, userAvatar, userPage));
-                }
+                };
             })
             .catch((error) => {
                 noLoader();
+                showError();
+                searchListElement.innerHTML = "";
                 console.log("Fetch error: ", error);
             });
+    } else {
+        searchListElement.innerHTML = "";
     };
 });
 
